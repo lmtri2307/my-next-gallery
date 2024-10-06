34a50c20b34ecff2ce0f27a52d37baffd9e2867e
20120634 <94401753+lmtri2307@users.noreply.github.com>
2024-10-07 01:22:47 +0700

    fix: antd menu items

--- my-next-gallery/app/ui/layout/header.tsx
+++ my-next-gallery/app/ui/layout/header.tsx
@@ -1,15 +1,16 @@
 import React from "react";
 import { Menu } from "antd";
 import { Header as AntdHeader } from "antd/es/layout/layout";
-import MenuItem from "antd/es/menu/MenuItem";
 
 export default function Header() {
   return (
     <AntdHeader>
       <div className="logo" />
-      <Menu theme="dark" mode="horizontal">
-        <MenuItem key="1">My Next Gallery</MenuItem>
-      </Menu>
+      <Menu
+        theme="dark"
+        mode="horizontal"
+        items={[{ key: "1", label: "My Next Gallery" }]}
+      />
     </AntdHeader>
   );
 }
