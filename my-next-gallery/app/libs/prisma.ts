f479b4a4e3d4511d07d81bfaccc8be012feae696
20120634 <94401753+lmtri2307@users.noreply.github.com>
2024-10-07 02:49:29 +0700

    Update prisma.ts

--- my-next-gallery/app/libs/prisma.ts
+++ my-next-gallery/app/libs/prisma.ts
@@ -13,6 +13,3 @@ const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
 export default prisma;
 
 if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
-
-// const prisma = new PrismaClient();
-// export default prisma;
